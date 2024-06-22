7a186f032aaaab4157608910e9128cb1a796a18a
arun <arun1nly1@gmail.com>
2024-06-22 14:15:23 +0545

    variable and d constant

---
+++ b.variable.and.constant.js
@@ -0,0 +1,53 @@
+// variable => whose value can be changed
+
+// ? syntax
+// let variableName = value;
+
+// let x = 2;
+
+// console.log(x);
+
+// let y = "Nepal";
+
+// y = "Canada";
+
+// console.log(y);
+
+// ? variable naming convention
+// ?1. variable name  must not include keyword
+// ? such as let, const, for, while, do, if, else ...
+
+// ? 2. variable name cannot start with numbers
+// ? eg. 1country, 1 => invalid
+// ? but num1, country1, student10 are valid
+
+// ?3. variable name cannot contain special characters
+// ? like !,@,#,%,^,&,* ...
+// ? except $, _
+
+// let num$ = 5;
+// console.log(num$);
+
+// let num_1 = 25;
+// console.log(num_1);
+
+// let _ = "Himalayan";
+// console.log(_);
+
+// let $ = "Element";
+// console.log($);
+
+// ? 4. Variable name should be descriptive
+
+// let temperature = 20;
+
+// ? 5. variable name should be camelCase
+
+// firstName, lastName, isMarried, hasSubscribed
+
+// ? constant
+// ? universal constant like PI must be uppercase
+
+const PI = 3.1414;
+
+const x = 5;
